9462f4ca4108ddca4045bf732cfcef853aab46eb
Manel <manel.castro39@gmail.com>
2021-03-25 13:47:30 +0100

    testimonials almost finished

--- src/components/Carousel/Carousel.elements.tsx
+++ src/components/Carousel/Carousel.elements.tsx
@@ -9,9 +9,8 @@ export const InfoContainer = styled(Container)`
 
 // D9C3CE
 export const InfoRow = styled.div`
-  display: flex-;
-  margin: -50px -50px 0px 0px;
-  right: 0px;
+  display: flex;
+  margin: -3.75rem -3rem 0px 0px;
   align-items: center;
   justify-content: flex-end;
   background: rgba(0, 0, 0, 0);
@@ -31,7 +30,7 @@ export const TextWrapper = styled.div`
   width: 300px;
   heigth: 500px;
   text-align: left;
-  padding: 30px 30px 60px 30px;
+  padding: 30px 30px 70px 30px;
   box-shadow: 2px 5px 10px lightgrey;
   width: 100%;
   height: 100%;
@@ -40,15 +39,46 @@ export const TextWrapper = styled.div`
 export const NameWrapper = styled.h3`
   color: black;
   font-size: 20px;
-  margin-top: -15px;
+  width: 100%;
   padding: 10px 0;
+  display: flex;
+  justify-content: flex-end;
 `;
 
 export const RatingWrapper = styled.div`
   height: 10px;
-  margin-bottom: -15px;
+  margin-top: -10px;
+  margin-bottom: 10px;
+  display: flex;
+  justify-content: flex-end;
 `;
 
 export const ImgWrapper = styled.div`
   display: flex;
+`;
+
+// Stars rating
+const starSize = '30px';
+const starColor = '#fff';
+const starBackground = '#fc0';
+
+export const Stars = styled.div`
+  display: inline-block;
+  font-size: ${starSize};
+  font-family: Times; /* make sure ★ appears correctly */
+  line-height: 1;
+
+  &::before {
+    content: '★★★★★';
+    letter-spacing: 3px;
+    background: linear-gradient(
+      90deg,
+      ${starColor}
+        ${({ rating }: { rating: number }) => `calc(${5 - rating} / 5 * 100%)`},
+      ${starBackground}
+        ${({ rating }: { rating: number }) => `calc(${5 - rating} / 5 * 100%)`}
+    );
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
 `;
--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -8,8 +8,10 @@ import {
   ImgWrapper,
   NameWrapper,
   RatingWrapper,
+  Stars,
 } from './Carousel.elements';
 import ratingImg1 from '../../media/señor.png';
+import ratingImg2 from '../../media/alba.png';
 
 // Comment:
 // this version of carousel is OK for computer, and very easily correct for mobile, but could be optimal TODO:
@@ -25,60 +27,77 @@ const _items = [
       rating: 5,
       opinion:
         'Muy buenos Veterinarios, cuidando mucho a las mascotas, todo muy bien explicado y muy atentos. Esterilizaron a mi gatita y todo perfecto, operación, chip, campana, las curas y medicamentos tal y como dijeron en un principio, siempre muy bien explicado y claro. Además de buenos y económicos tienen mucho cuidado con los animales, no soy de Barcelona, pero vale la pena hacer unos kilómetros por la atención y profesionalidad que tienen. Sin duda los mejores !!!',
+      opinionLink: 'https://goo.gl/maps/bsvpirMac4uJhJcV7',
     },
   },
   {
     player: {
-      title: "2 Ronnie O'Sullivan",
-      desc:
-        "Ronald Antonio O'Sullivan is a six-time world champion and is the most successful player in the history of snooker.",
-      image: 'https://i.postimg.cc/qBGQNc37/ro-slider.jpg',
+      name: 'Cesar Vidal',
+      image: ratingImg2,
+      rating: 5,
+      opinion:
+        'Muy buenos Veterinarios, cuidando mucho a las mascotas, todo muy bien explicado y muy atentos. Esterilizaron a mi gatita y todo perfecto, operación, chip, campana, las curas y medicamentos tal y como dijeron en un principio, siempre muy bien explicado y claro. Además de buenos y económicos tienen mucho cuidado con los animales, no soy de Barcelona, pero vale la pena hacer unos kilómetros por la atención y profesionalidad que tienen. Sin duda los mejores !!!',
+      opinionLink: 'https://goo.gl/maps/bsvpirMac4uJhJcV7',
     },
   },
   {
     player: {
-      title: '3 Shane Van Boening',
-      desc:
-        'The "South Dakota Kid" is hearing-impaired and uses a hearing aid, but it has not limited his ability.',
-      image: 'https://i.postimg.cc/cHdMJQKG/svb-slider.jpg',
+      name: 'Cesar Vidal',
+      image: ratingImg1,
+      rating: 5,
+      opinion:
+        'Muy buenos Veterinarios, cuidando mucho a las mascotas, todo muy bien explicado y muy atentos. Esterilizaron a mi gatita y todo perfecto, operación, chip, campana, las curas y medicamentos tal y como dijeron en un principio, siempre muy bien explicado y claro. Además de buenos y económicos tienen mucho cuidado con los animales, no soy de Barcelona, pero vale la pena hacer unos kilómetros por la atención y profesionalidad que tienen. Sin duda los mejores !!!',
+      opinionLink: 'https://goo.gl/maps/bsvpirMac4uJhJcV7',
     },
   },
   {
     player: {
-      title: '4 Mike Sigel',
-      desc:
-        'Mike Sigel or "Captain Hook" as many like to call him is an American professional pool player with over 108 tournament wins.',
-      image: 'https://i.postimg.cc/C12h7nZn/ms-1.jpg',
+      name: 'Cesar Vidal',
+      image: ratingImg1,
+      rating: 5,
+      opinion:
+        'Muy buenos Veterinarios, cuidando mucho a las mascotas, todo muy bien explicado y muy atentos. Esterilizaron a mi gatita y todo perfecto, operación, chip, campana, las curas y medicamentos tal y como dijeron en un principio, siempre muy bien explicado y claro. Además de buenos y económicos tienen mucho cuidado con los animales, no soy de Barcelona, pero vale la pena hacer unos kilómetros por la atención y profesionalidad que tienen. Sin duda los mejores !!!',
+      opinionLink: 'https://goo.gl/maps/bsvpirMac4uJhJcV7',
     },
   },
   {
     player: {
-      title: '5 Willie Mosconi',
-      desc:
-        'Nicknamed "Mr. Pocket Billiards," Willie Mosconi was among the first Billiard Congress of America Hall of Fame inductees.',
-      image: 'https://i.postimg.cc/NfzMDVHP/willie-mosconi-slider.jpg',
+      name: 'Cesar Vidal',
+      image: ratingImg1,
+      rating: 5,
+      opinion:
+        'Muy buenos Veterinarios, cuidando mucho a las mascotas, todo muy bien explicado y muy atentos. Esterilizaron a mi gatita y todo perfecto, operación, chip, campana, las curas y medicamentos tal y como dijeron en un principio, siempre muy bien explicado y claro. Además de buenos y económicos tienen mucho cuidado con los animales, no soy de Barcelona, pero vale la pena hacer unos kilómetros por la atención y profesionalidad que tienen. Sin duda los mejores !!!',
+      opinionLink: 'https://goo.gl/maps/bsvpirMac4uJhJcV7',
     },
   },
 ];
 
-const CarouselItem = () => {
+interface CarouselItemProps {
+  rating: number;
+  opinion: string;
+  name: string;
+  image: any;
+  opinionLink?: string;
+}
+
+const CarouselItem = ({ rating, opinion, name, image }: CarouselItemProps) => {
   return (
     <InfoContainer>
       <InfoColumn>
         <TextWrapper>
-          <div>{_items[0].player.opinion}</div>
+          <div>{opinion}</div>
         </TextWrapper>
         <InfoRow>
           <InfoColumn>
-            <NameWrapper>
-              <div>{_items[0].player.name}</div>
-            </NameWrapper>
             <RatingWrapper>
-              <div>{_items[0].player.rating}</div>
+              <Stars rating={rating} />
             </RatingWrapper>
+            <NameWrapper>
+              <div>{name}</div>
+            </NameWrapper>
           </InfoColumn>
           <ImgWrapper>
-            <img src={_items[0].player.image}></img>
+            <img src={image}></img>
           </ImgWrapper>
         </InfoRow>
       </InfoColumn>
@@ -88,7 +107,7 @@ const CarouselItem = () => {
 
 const Carousel = () => {
   useEffect(() => {
-    // setInterval(changeOrder, 4000);
+    setInterval(changeOrder, 4000);
   });
   const [items, setItems] = useState(_items);
 
@@ -114,19 +133,44 @@ const Carousel = () => {
   return (
     <div className="all-slides">
       <div className="single-slide" data-order="1">
-        <CarouselItem />
+        <CarouselItem
+          rating={_items[0].player.rating!}
+          opinion={_items[0].player.opinion}
+          name={_items[0].player.name}
+          image={_items[0].player.image}
+        />
       </div>
       <div className="single-slide" data-order="2">
-        <CarouselItem />
+        <CarouselItem
+          rating={_items[1].player.rating!}
+          opinion={_items[1].player.opinion}
+          name={_items[1].player.name}
+          image={_items[1].player.image}
+        />{' '}
       </div>
       <div className="single-slide" data-order="3">
-        <CarouselItem />
+        <CarouselItem
+          rating={_items[0].player.rating!}
+          opinion={_items[0].player.opinion}
+          name={_items[0].player.name}
+          image={_items[0].player.image}
+        />{' '}
       </div>
       <div className="single-slide" data-order="4">
-        <CarouselItem />
+        <CarouselItem
+          rating={_items[0].player.rating!}
+          opinion={_items[0].player.opinion}
+          name={_items[0].player.name}
+          image={_items[0].player.image}
+        />{' '}
       </div>
       <div className="single-slide" data-order="5">
-        <CarouselItem />
+        <CarouselItem
+          rating={_items[0].player.rating!}
+          opinion={_items[0].player.opinion}
+          name={_items[0].player.name}
+          image={_items[0].player.image}
+        />{' '}
       </div>
     </div>
   );
